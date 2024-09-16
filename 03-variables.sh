4fb33baadcee62e6b5cd7fb152afee21f906b5f4
yravula <yravula@gmail.com>
2024-09-16 11:28:01 +0530

    updated variables with dynamic update

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,9 +7,6 @@ DATE=(date +%F)
 
 echo Good Morning, Welcome to today class on $DATE
 
-no_of_users =$(who | wc -l)
+no_of_users=$(who | wc -l)
 echo Number of users logged into the system = $no_of_users
 
-#test vaules on git pull update on EC2
-
-#git config pull.rebase true
