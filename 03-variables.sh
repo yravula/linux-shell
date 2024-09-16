178bac417c1637d1cd6561ed8cec42aa94e43cfd
yravula <yravula@gmail.com>
2024-09-16 11:12:00 +0530

    update values

--- 03-variables.sh
+++ 03-variables.sh
@@ -10,3 +10,4 @@ echo Good Morning, Welcome to today class on $DATE
 no_of_users =$(who | wc -l)
 echo Number of users logged into the system = $no_of_users
 
+#test vaules on git pull update on EC2
