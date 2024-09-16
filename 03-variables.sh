5d0472c66f2801a834c9e6d5a22a4426664049e9
yravula <yravula@gmail.com>
2024-09-16 11:41:52 +0530

    update the file

--- 03-variables.sh
+++ 03-variables.sh
@@ -10,3 +10,4 @@ echo Good Morning, Welcome to today class on $DATE
 no_of_users=$(who | wc -l)
 echo Number of users logged into the system = $no_of_users
 
+# git pull not updating the latest update from git hub
