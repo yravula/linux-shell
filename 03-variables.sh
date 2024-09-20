59219fe69c4776394f746365475adaa20dbd0502
yravula <yravula@gmail.com>
2024-09-20 10:25:01 +0530

    added varilable environment

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,8 +5,7 @@ echo course name = $COURSE
 #dynamic date call
 DATE=(date +%F)
 
-echo Good Morning, Welcome to today class on $DATE
-
+echo Good Morning, Welcome to today class on Date = $DATE
 no_of_users=$(who | wc -l)
 echo Number of users logged into the system = $no_of_users
 
@@ -15,3 +14,5 @@ MULTIPLY=$((2*3*4))
 
 echo ADD = $ADD
 echo MULTIPLY = $MULTIPLY
+
+echo variable from cmd line x =$x
