b72024a69091f29bccc88e9b284184c1bdfc784a
yravula <yravula@gmail.com>
2024-09-20 11:05:12 +0530

    echo variables

---
+++ 05.special-vars-input.sh
@@ -0,0 +1,18 @@
+#to get script name
+echo $0
+
+#First argument to the script
+echo $1
+
+#sceond argument to the script 
+echo $2
+
+# like wise you can go till $n
+
+# For all the arguments parsed
+
+echo $*
+echo $@
+
+#number of arguments
+
